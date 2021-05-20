98b1e855fb29d09026475c7414b83b5f71be284c
Armin Shoeibi <68565441+ArminShoeibi@users.noreply.github.com>
2021-05-21 02:13:30 +0430

    Add project files.

---
+++ Flights.Client/Program.cs
@@ -0,0 +1,40 @@
+﻿using Flights.Messages;
+using Google.Protobuf;
+using NetMQ;
+using NetMQ.Sockets;
+using System;
+using System.Threading.Tasks;
+
+namespace Flights.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using RequestSocket requestSocket = new(">tcp://localhost:5500");
+
+            CreateFlightDto createFlightDto = new()
+            {
+                TravelClass = TravelClass.FirstClass,
+                Airline = "SwissAir",
+                Destination = "THR",
+                Origin = "LX",
+                FlightNumber = "SW908",
+                ChildFare = 200,
+                InfantFare = 180,
+                AdultFare = 240,
+
+            };
+            byte[] createFlightDtoBytes = createFlightDto.ToByteArray();
+            requestSocket.SendFrame(createFlightDtoBytes);
+
+            byte[] createFlightResultBytes = requestSocket.ReceiveFrameBytes();
+            CreateFlightResultDto createFlightResultDto = CreateFlightResultDto.Parser.ParseFrom(createFlightResultBytes);
+
+            Console.WriteLine($"Message: {createFlightResultDto.IsSuccessful}");
+            Console.WriteLine($"Message: {createFlightResultDto.Message}");
+
+
+        }
+    }
+}
---
+++ Flights.Server/Program.cs
@@ -0,0 +1,30 @@
+﻿using Flights.Messages;
+using Google.Protobuf;
+using NetMQ;
+using NetMQ.Sockets;
+using System;
+
+namespace Flights.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using ResponseSocket responseSocket = new("@tcp://localhost:5500");
+
+            byte[] createFlightDtoBytes = responseSocket.ReceiveFrameBytes();
+
+            CreateFlightDto createFlightDto =
+                CreateFlightDto.Parser.ParseFrom(createFlightDtoBytes);
+
+            CreateFlightResultDto createFlightResultDto = new()
+            {
+                IsSuccessful = true,
+                Message = "Flight was successfully added."
+            };
+            byte[] createFlightResultDtoBytes = createFlightResultDto.ToByteArray();
+
+            responseSocket.SendFrame(createFlightResultDtoBytes, false);
+        }
+    }
+}
